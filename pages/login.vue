cc44ba13cd617c96c3cfa3d07a97632be1baab47
shimar <shima01dev@gmail.com>
2018-03-30 16:18:35 +0900

    Add login feature.

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,38 @@
 <template lang="pug">
 .container
   h1 login
+  input(type='button'
+        value='Login in with Google'
+        @click='doLogin')
 </template>
 
 <script>
+import firebase from '@/plugins/firebase'
+import { mapActions } from 'vuex'
+
 export default {
+  async mounted () {
+    let user = await new Promise((resolve, reject) => {
+      firebase.auth().onAuthStateChanged((user) => resolve(user))
+    })
+    this.setUser(user) // setUser is mapped action from vuex
+    if (user) {
+      this.$router.push('/') // if non-null user given, go to root page.
+    }
+  },
+
+  methods: {
+    ...mapActions([
+      'login',
+      'setUser'
+    ]),
+
+    doLogin () {
+      this.login()
+        .then(() => console.log('resloved'))
+        .catch((err) => console.log(err))
+    }
+  }
 }
 </script>
 

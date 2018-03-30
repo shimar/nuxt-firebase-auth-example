c579e9d8ddf52700d72261040cd64c032448cee3
shimar <shima01dev@gmail.com>
2018-03-30 16:18:56 +0900

    Add logout feature.

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,28 @@
 <template lang="pug">
 .container
   h1 index
+  input(type='button'
+        value='Logout'
+        @click='doLogout')
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
+  methods: {
+    ...mapActions([
+      'logout'
+    ]),
+
+    doLogout () {
+      this.logout()
+        .then(() => {
+          this.$router.push('/login')
+        })
+        .catch((err) => console.log(err))
+    }
+  }
 }
 </script>
 

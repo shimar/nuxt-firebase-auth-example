cefb0c7135ccd6c7507960f0f49ff414cbc8dd7f
shimar <shima01dev@gmail.com>
2018-03-30 16:18:12 +0900

    Add some mutations and actions.

--- store/index.js
+++ store/index.js
@@ -1,6 +1,42 @@
+import firebase from '@/plugins/firebase'
+
+const googleProvider = new firebase.auth.GoogleAuthProvider()
+
+export const strict = false
+
 export const state = () => ({
   user: null
 })
+
+export const mutations = {
+  setUser (state, payload) {
+    state.user = payload
+  }
+}
+
+export const actions = {
+  login () {
+    return new Promise((resolve, reject) => {
+      firebase.auth().signInWithRedirect(googleProvider)
+        .then(() => resolve())
+        .catch((err) => reject(err))
+    })
+  },
+
+  logout ({ commit }) {
+    return new Promise((resolve, reject) => {
+      firebase.auth().signOut()
+        .then(() => {
+          commit('setUser', null)
+          resolve()
+        })
+    })
+  },
+
+  setUser ({ commit }, payload) {
+    commit('setUser', payload)
+  }
+}
 
 export const getters = {
   isAuthenticated (state) {
